07d8d56d9348a1dbea315e4886d40a083269696b
gimangi <khk211113@naver.com>
2022-07-28 22:09:52 +0900

    5052 전화번호 목록

---
+++ BOJ/java/src/BOJ_1911/Main.java
@@ -0,0 +1,2 @@
+package BOJ_1911;public class Main {
+}
---
+++ BOJ/java/src/BOJ_5052/Main.java
@@ -0,0 +1,46 @@
+package BOJ_5052;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            ArrayList<String> list = new ArrayList<>();
+            for (int i=0; i<n; i++) {
+                list.add(br.readLine());
+            }
+
+            Collections.sort(list);
+
+            if (isAccept(list))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+
+        }
+
+    }
+
+    public static boolean isAccept(final ArrayList<String> list) {
+        for (int i=0; i<list.size()-1; i++) {
+            String prev = list.get(i);
+            String next = list.get(i+1);
+
+            if (next.length() >= prev.length() && next.substring(0, prev.length()).equals(prev))
+                    return false;
+
+        }
+        return true;
+    }
+}
