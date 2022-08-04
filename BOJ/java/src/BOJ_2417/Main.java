77a3f57a61a38fb93198cb3f67eb69a5bee8e98a
gimangi <khk211113@naver.com>
2022-08-05 03:31:55 +0900

    2417 정수 제곱근

--- BOJ/java/src/BOJ_2417/Main.java
+++ BOJ/java/src/BOJ_2417/Main.java
@@ -1,2 +1,29 @@
-package BOJ_2417;public class Main {
+package BOJ_2417;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long ans = Long.MAX_VALUE;
+
+        long start = 0, end = n;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+
+            if ((long) Math.pow(mid, 2) >= n) {
+                ans = Math.min(mid, ans);
+
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+        }
+        System.out.println(ans);
+
+     }
 }
