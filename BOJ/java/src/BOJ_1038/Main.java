2caf9c822cdade7e397f9def3cd6d013f5040eed
gimangi <khk211113@naver.com>
2022-08-04 14:38:48 +0900

    1911 흙길 보수하기

---
+++ BOJ/java/src/BOJ_1038/Main.java
@@ -0,0 +1,2 @@
+package BOJ_1038;public class Main {
+}
--- BOJ/java/src/BOJ_1911/Main.java
+++ BOJ/java/src/BOJ_1911/Main.java
@@ -1,2 +1,56 @@
-package BOJ_1911;public class Main {
+package BOJ_1911;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    static int N, L;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Pool> poolList = new ArrayList<>();
+
+        String[] line = br.readLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        L = Integer.parseInt(line[1]);
+
+        for (int i = 0; i < N; i++) {
+            line = br.readLine().split(" ");
+            Pool pool = new Pool(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
+            poolList.add(pool);
+        }
+
+        Collections.sort(poolList, (p1, p2) -> p1.start - p2.start);
+
+        int ans = 0;
+        int range = 0;
+        for (int i=0; i<N; i++) {
+            Pool pool = poolList.get(i);
+
+            if (pool.start > range)
+                range = pool.start;
+            while (pool.end > range) {
+                range += L;
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
+
+class Pool {
+    public int start;
+    public int end;
+
+    Pool(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
 }
