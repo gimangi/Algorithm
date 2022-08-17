3d6dc075184e6c6eca07617ee1f3e8c563740e07
gimangi <khk211113@naver.com>
2022-08-17 19:36:43 +0900

    1799 비숍

--- BOJ/java/src/BOJ_1799/Main.java
+++ BOJ/java/src/BOJ_1799/Main.java
@@ -16,8 +16,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new boolean[N][N];
-        int[][] placedBlack = new int[N][N];
-        int[][] placedWhite = new int[N][N];
+        int[] diagDown = new int[2 * N - 1];
+        int[] diagUp = new int[2 * N - 1];
         ArrayList<Position> posBlack = new ArrayList<>();
         ArrayList<Position> posWhite = new ArrayList<>();
 
@@ -33,41 +33,33 @@ public class Main {
                 }
             }
         }
-
         int ans = 0;
         if (!posBlack.isEmpty())
-            ans += dfs(posBlack, placedBlack, 0, 0);
+            ans += dfs(posBlack, diagUp, diagDown, 0, 0);
         if (!posWhite.isEmpty())
-            ans += dfs(posWhite, placedWhite, 0, 0);
+            ans += dfs(posWhite, diagUp, diagDown, 0, 0);
 
         System.out.println(ans);
     }
 
-    static int dfs(final ArrayList<Position> positions, final int[][] placed, int idx, int cnt) {
+    static int dfs(final ArrayList<Position> positions, final int[] diagUp, final int[] diagDown, int idx, int cnt) {
         if (!(idx < positions.size())) return cnt;
         int ret = 0;
 
         Position cur = positions.get(idx);
-        if (placed[cur.y][cur.x] == 0) {
-            addPlace(placed, cur.y, cur.x, 1);
-            ret = Math.max(dfs(positions, placed, idx + 1, cnt + 1), ret);
-            addPlace(placed, cur.y, cur.x, -1);
+        int diagUpIdx = cur.y - cur.x + N - 1;
+        int diagDownIdx = cur.y + cur.x;
+
+        if (diagUp[diagUpIdx] == 0 && diagDown[diagDownIdx] == 0) {
+            diagUp[diagUpIdx] += 1;
+            diagDown[diagDownIdx] += 1;
+            ret = Math.max(dfs(positions, diagUp, diagDown, idx + 1, cnt + 1), ret);
+            diagUp[diagUpIdx] -= 1;
+            diagDown[diagDownIdx] -= 1;
         }
 
-        ret = Math.max(dfs(positions, placed, idx + 1, cnt), ret);
+        ret = Math.max(dfs(positions, diagUp, diagDown, idx + 1, cnt), ret);
         return ret;
-    }
-
-    static void addPlace(final int[][] placed, int y, int x, int num) {
-        placed[y][x] += num;
-        for (int i = 1; y - i >= 0 && x + i < N; i++)
-            placed[y - i][x + i] += num;
-        for (int i = 1; y - i >= 0 && x - i >= 0; i++)
-            placed[y - i][x - i] += num;
-        for (int i = 1; y + i < N && x - i >= 0; i++)
-            placed[y + i][x - i] += num;
-        for (int i = 1; y + i < N && x + i < N; i++)
-            placed[y + i][x + i] += num;
     }
 
     static class Position {
